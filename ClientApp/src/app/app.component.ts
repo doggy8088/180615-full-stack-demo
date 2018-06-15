35e20353fb1a2396f5c4c4ec476270738c19a3f6
Will <doggy.huang@gmail.com>
2018-06-16 01:08:01 +0800

    改用 ES6 的 String Literal 讓字串組合更精簡

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent {
       debounceTime(500),
       mergeMap(evt => {
         let [k, z, t] = evt;
-        return this.http.get('/api/spots?k=' + k + '&z=' + z + '&t=' + t);
+        return this.http.get(`/api/spots?k=${k}&z=${z}&t=${t}`);
       })
     );
   }
